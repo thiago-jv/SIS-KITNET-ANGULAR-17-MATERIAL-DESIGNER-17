9963c7dc2cb7124d709c55a3f5111430ff102d65
Thiago <thiago.henrique.25@hotmail.com>
2025-11-14 20:24:59 -0300

    feat: criando layout inicial

--- sis-kitnet/src/app/app.component.ts
+++ sis-kitnet/src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { CommonModule } from '@angular/common';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sis-kitnet';
+
+  isMobile = false;
+
+  constructor(private breakpoints: BreakpointObserver) {
+    this.breakpoints
+      .observe(['(max-width: 768px)'])
+      .subscribe(result => this.isMobile = result.matches);
+  }
 }
