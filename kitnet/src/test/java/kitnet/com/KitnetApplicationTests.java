df50d7d2a674375be79668c45544db620483cc65
Thiago Henrique <thiago.henrique.25@hotmail.com>
2025-11-17 09:10:58 -0500

    feat: ajustando configurações inicial projeto backend

---
+++ kitnet/src/test/java/kitnet/com/KitnetApplicationTests.java
@@ -0,0 +1,13 @@
+package kitnet.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
