528d95c3fb5f6698b0dcf51d02b362aa6751b976
Thiago Henrique <thiago.henrique.25@hotmail.com>
2025-11-18 10:20:56 -0500

    feat: ajustando para portugues os labels de paginacao de ingles para portugues com uma classe utilitaria.

--- sis-kitnet/src/app/app.config.ts
+++ sis-kitnet/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {HttpClientModule} from "@angular/common/http";
+import {MatPaginatorIntl} from "@angular/material/paginator";
+import {PortuguesePaginatorIntl} from "./util/portuguesePaginatorIntl";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    { provide: MatPaginatorIntl, useClass: PortuguesePaginatorIntl }
   ]
 };
