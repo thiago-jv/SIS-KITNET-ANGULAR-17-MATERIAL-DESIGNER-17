3da3dfdb2e2e2bd5647d198caaf12665f885ed92
Thiago Henrique <thiago.henrique.25@hotmail.com>
2026-04-02 17:35:14 -0400

    feat: Liberando chamada para "/actuator/health"

--- kitnet/src/main/java/kitnet/com/security/SecurityConfig.java
+++ kitnet/src/main/java/kitnet/com/security/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                         "/v1/auth/login",
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
-                        "/swagger-ui.html"
+                        "/swagger-ui.html",
+                        "/actuator/health"
                     ).permitAll()
                     .anyRequest().authenticated()
                 )
